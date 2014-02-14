dfbd535555f0b8540da0973091cabe4d1ccdc043
Sungcheol Kim <skyfe79@gmail.com>
2014-02-14 15:09:34 +0900

    Adding SDL & FFmpeg library

---
+++ HelloFFmpeg/main.mm
@@ -0,0 +1,19 @@
+//
+//  main.mm
+//  HelloFFmpeg
+//
+//  Created by burt on 2014. 2. 13..
+//  Copyright (c) 2014년 burt. All rights reserved.
+//
+
+#include "main.h"
+#include <SDL/SDL.h>
+#include "SDLMain.h"
+#include <libavcodec/avcodec.h>
+
+int main(int argc, char **argv)
+{
+	
+	
+	return 0;
+}
