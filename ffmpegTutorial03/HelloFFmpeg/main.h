509182168b66753292d3f49d9b2f43cbe496a308
Sungcheol Kim <skyfe79@gmail.com>
2014-02-16 12:23:22 +0900

    Adding ffmpegTutorial03

---
+++ ffmpegTutorial03/HelloFFmpeg/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  HelloFFmpeg
+//
+//  Created by burt on 2014. 2. 13..
+//  Copyright (c) 2014년 burt. All rights reserved.
+//
+
+#ifndef __HelloFFmpeg__main__
+#define __HelloFFmpeg__main__
+
+#include <iostream>
+
+#endif /* defined(__HelloFFmpeg__main__) */
